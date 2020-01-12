bc5170bcdb59d3a5d302ec0f8d3ed29931af7f3c
Matthew Saltz <matthew.saltz@mongodb.com>
2020-01-12 16:42:43 -0500

    All kinds of future chain working

--- main.cpp
+++ main.cpp
@@ -322,23 +322,7 @@ class Future {
             std::forward<ThisType>(*this));
     }
 
-    //    auto spawn(Executor &executor) {
-    //        if constexpr (std::is_null_pointer<InputChain>::value) {
-    //            executor.schedule([this] { return _callable(); });
-    //        } else {
-    //            if constexpr (std::is_void<decltype(_inputChain.go())>::value)
-    //            {
-    //                _inputChain.goExec(executor);
-    //                executor.schedule([this] { return _callable(); });
-    //
-    //                // return _callable();
-    //            } else {
-    //                return _callable(_inputChain.goExec(executor));
-    //            }
-    //        }
-    //    }
-
-    auto goOrig() {
+    auto go() {
         if constexpr (std::is_null_pointer<InputChain>::value) {
             return _callable();
         } else {
@@ -373,106 +357,68 @@ class Future {
 
     template <typename Callback>
     auto goImpl(Executor &executor, Callback &&callback) {
-        // std::cout << "XXX 0" << std::endl;
         if constexpr (std::is_null_pointer<InputChain>::value) {
-            //   std::cout << "XXX 1" << std::endl;
             if constexpr (std::is_void<decltype(_callable())>::value) {
-                //      std::cout << "XXX 2" << std::endl;
                 _callable();
                 executor.schedule([this, &executor, callback]() mutable {
-                    //         std::cout << "XXX 3" << std::endl;
                     return callback();
                 });
             } else {
-                //    std::cout << "XXX 4" << std::endl;
                 auto result = _callable();
                 executor.schedule([this, &executor, callback,
                                    result = std::move(result)]() mutable {
-                    //       std::cout << "XXX 5" << std::endl;
                     return callback(std::move(result));
                 });
             }
         } else {
-            // std::cout << "XXX 6" << std::endl;
-            if constexpr (std::is_void<decltype(_inputChain.go())>::value) {
-                //   std::cout << "XXX 7" << std::endl;
-                return _inputChain.goImpl(executor, [this, &executor,
-                                                     callback]() mutable {
-                    //      std::cout << "XXX 8" << std::endl;
-                    auto result = _callable();
-                    //     std::cout << "XXX 9" << std::endl;
-                    executor.schedule([this, &executor, callback,
-                                       result = std::move(result)]() mutable {
-                        //        std::cout << "XXX 10" << std::endl;
-                        return callback(std::move(result));
-                    });
-                });
+            if constexpr (std::is_invocable_v<MyCallable>) {
+                if constexpr (std::is_invocable_v<Callback>) {
+                    return _inputChain.goImpl(
+                        executor, [this, &executor, callback]() mutable {
+                            _callable();
+                            executor.schedule(
+                                [this, &executor, callback]() mutable {
+                                    return callback();
+                                });
+                        });
+                } else {
+                    return _inputChain.goImpl(
+                        executor, [this, &executor, callback]() mutable {
+                            auto result = _callable();
+                            executor.schedule(
+                                [this, &executor, callback,
+                                 result = std::move(result)]() mutable {
+                                    return callback(std::move(result));
+                                });
+                        });
+                }
             } else {
-                // std::cout << "XXX 11" << std::endl;
-                return _inputChain.goImpl(
-                    executor,
-                    [this, &executor, callback](auto &&inputResult) mutable {
-                        //       std::cout << "XXX 12" << std::endl;
-                        auto result = _callable(
-                            std::forward<decltype(inputResult)>(inputResult));
-                        //      std::cout << "XXX 13" << std::endl;
-                        executor.schedule(
-                            [this, &executor, callback,
-                             result = std::move(result)]() mutable {
-                                //             std::cout << "XXX 14" <<
-                                //             std::endl;
-                                return callback(std::move(result));
-                            });
-                    });
-            }
-        }
-    }
+                if constexpr (std::is_invocable_v<Callback>) {
+                    return _inputChain.goImpl(
+                        executor, [this, &executor,
+                                   callback](auto &&inputResult) mutable {
+                            _callable(std::forward<decltype(inputResult)>(
+                                inputResult));
+                            executor.schedule(
+                                [this, &executor, callback]() mutable {
+                                    return callback();
+                                });
+                        });
 
-    auto go() {
-        if constexpr (std::is_null_pointer<InputChain>::value) {
-            return _callable();
-        } else {
-            if constexpr (std::is_void<decltype(_inputChain.go())>::value) {
-                return _inputChain.goImpl(
-                    [this]() mutable { return _callable(); });
-            } else {
-                return _inputChain.goImpl([this](auto &&inputResult) mutable {
-                    return _callable(
-                        std::forward<decltype(inputResult)>(inputResult));
-                });
-            }
-        }
-    }
-
-    template <typename Callback>
-    auto goImpl(Callback &&callback) {
-        // std::cout << "XXX 15" << std::endl;
-        if constexpr (std::is_null_pointer<InputChain>::value) {
-            if constexpr (std::is_void<decltype(_callable())>::value) {
-                //       std::cout << "XXX 16" << std::endl;
-                _callable();
-                //      std::cout << "XXX 17" << std::endl;
-                return callback();
-            } else {
-                //     std::cout << "XXX 18" << std::endl;
-                return callback(_callable());
-            }
-        } else {
-            // std::cout << "XXX 19" << std::endl;
-            if constexpr (std::is_void<decltype(_inputChain.go())>::value) {
-                //   std::cout << "XXX 20" << std::endl;
-                return _inputChain.goImpl([this, callback]() mutable {
-                    //      std::cout << "XXX 21" << std::endl;
-                    return callback(_callable());
-                });
-            } else {
-                // std::cout << "XXX 22" << std::endl;
-                return _inputChain.goImpl(
-                    [this, callback](auto &&inputResult) mutable {
-                        //        std::cout << "XXX 23" << std::endl;
-                        return callback(_callable(
-                            std::forward<decltype(inputResult)>(inputResult)));
-                    });
+                } else {
+                    return _inputChain.goImpl(
+                        executor, [this, &executor,
+                                   callback](auto &&inputResult) mutable {
+                            auto result =
+                                _callable(std::forward<decltype(inputResult)>(
+                                    inputResult));
+                            executor.schedule(
+                                [this, &executor, callback,
+                                 result = std::move(result)]() mutable {
+                                    return callback(std::move(result));
+                                });
+                        });
+                }
             }
         }
     }
@@ -503,31 +449,50 @@ auto makeTestFutureChain2(Executor &executor) {
             std::cout << "input s:  " << s << std::endl;
         });
 }
+
+// auto voidThenValue(Executor &executor) {
+//    return makeFuture([]() { std::cout << "step 0" << std::endl; })
+//        .then([&executor]() {
+//            std::cout << "step 1" << std::endl;
+//            return 2;
+//        })
+//        .then([](int &&i) {
+//            //           std::cout << "step 2" << std::endl;
+//            std::cout << "in a future with i : " << i << std::endl;
+//            return std::string("something");
+//        })
+//        .then([](std::string &&s) {
+//            //            std::cout << "step 3" << std::endl;
+//            std::cout << "input s:  " << s << std::endl;
+//        })
+//        .then([](std::string &&s) {
+//            //            std::cout << "step 3" << std::endl;
+//            std::cout << "input s:  " << s << std::endl;
+//        })
+//        .go(executor);
+//}
+
 auto makeTestFutureChain(Executor &executor) {
     return makeFuture([]() { std::cout << "step 0" << std::endl; })
         .then([&executor]() {
             std::cout << "step 1" << std::endl;
             makeFuture([]() {
-                std::cout << "step million0" << std::endl;
+                std::cout << "step 1.1" << std::endl;
                 return 4;
             })
-                .then(
-                    [](int &&i) { std::cout << "step million1" << std::endl; })
-                //.then([]() { std::cout << "step million2" << std::endl; })
+                .then([](int &&i) { std::cout << "step 1.2" << std::endl; })
                 .go(executor);
             return 2;
         })
         .then([](int &&i) {
-            //           std::cout << "step 2" << std::endl;
-            std::cout << "in a future with i : " << i << std::endl;
+            std::cout << "step 2, i : " << i << std::endl;
             return std::string("something");
         })
         .then([](std::string &&s) {
-            //            std::cout << "step 3" << std::endl;
-            std::cout << "input s:  " << s << std::endl;
-        });
+            std::cout << "step 3, s:  " << s << std::endl;
+        })
+        .then([]() { std::cout << "step 4" << std::endl; });
 }
-
 int main() {
     // std::cout << "Hello World!\n";
 
@@ -536,34 +501,33 @@ int main() {
         makeTestFutureChain(executor).go(executor);
     }
 
-    // for (int i = 0; i < 100000; ++i) {
-    //    std::cout << "Something asdfasdf" << i << std::endl;
-    //    std::cout << "Something asdfasdf" << i << std::endl;
-    //    std::cout << "Something asdfasdf" << i << std::endl;
-    //    std::cout << "Something asdfasdf" << i << std::endl;
-    //    std::cout << "Something asdfasdf" << i << std::endl;
-    //}
-
-    //    makeTestFutureChain(executor).go(executor);
-    //    makeTestFutureChain(executor).go(executor);
-
     executor.run();
-
-    // makeFuture()
-    //    .then([]() {
-    //        std::cout << "step 1" << std::endl;
-    //        return 2;
-    //    })
-    //    .then([](int i) {
-    //        std::cout << "in a future with i : " << i << std::endl;
-    //    })
-    //    .then([] {
-    //        std::cout << "in a continuation:  " << std::endl;
-    //        return std::string("something");
-    //    })
-    //    .then(
-    //        [](std::string &s) { std::cout << "input s:  " << s <<
-    //        std::endl;
-    //        })
-    //    .go();
 }
+// for (int i = 0; i < 100000; ++i) {
+//    std::cout << "Something asdfasdf" << i << std::endl;
+//    std::cout << "Something asdfasdf" << i << std::endl;
+//    std::cout << "Something asdfasdf" << i << std::endl;
+//    std::cout << "Something asdfasdf" << i << std::endl;
+//    std::cout << "Something asdfasdf" << i << std::endl;
+//}
+
+//    makeTestFutureChain(executor).go(executor);
+//    makeTestFutureChain(executor).go(executor);
+
+// makeFuture()
+//    .then([]() {
+//        std::cout << "step 1" << std::endl;
+//        return 2;
+//    })
+//    .then([](int i) {
+//        std::cout << "in a future with i : " << i << std::endl;
+//    })
+//    .then([] {
+//        std::cout << "in a continuation:  " << std::endl;
+//        return std::string("something");
+//    })
+//    .then(
+//        [](std::string &s) { std::cout << "input s:  " << s <<
+//        std::endl;
+//        })
+//    .go();
