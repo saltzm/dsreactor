3172d2be841d372f67d13d2f46ca344cc9a5cd54
Matthew Saltz <matthew.saltz@mongodb.com>
2020-01-13 10:22:40 -0500

    Remove commented stuff

--- main.cpp
+++ main.cpp
@@ -2,289 +2,11 @@
 #include <list>
 #include <type_traits>
 
-/*
-template <typename InputCallable, typename... Callables>
-class Future : public Future<Callables...> {
-public:
-    Future(InputCallable &&input, Callables... callables) :
-_input(std::move(input)) {}
-
-    template <typename Continuation>
-    auto then(Continuation && callable) {
-        return Future<InputCallable, Continuation>(std::move(_input),
-std::move(callable));
-    }
-
-private:
-    InputCallable _input;
-};
-*/
-
-// template <typename InputCallable, typename InputCallable2>
-// class Future2;
-//
-// template <typename InputCallable>
-// class Future {
-//   public:
-//    Future(InputCallable &&input) : _input(std::move(input)) {}
-//
-//    template <typename Continuation>
-//    auto then(Continuation &&callable) {
-//        return Future2<InputCallable, Continuation>(std::move(_input),
-//                                                    std::move(callable));
-//    }
-//
-//    auto go() { return _input(); }
-//
-//   private:
-//    InputCallable _input;
-//};
-//
-// template <typename InputCallable, typename InputCallable2>
-// class Future2 : public Future<InputCallable> {
-//   public:
-//    Future2(InputCallable &&input, InputCallable2 &&input2)
-//        : Future<InputCallable>(std::move(input)), _input2(std::move(input2))
-//        {}
-//
-//    /*
-//    template <typename Continuation>
-//    auto then(Continuation && callable) {
-//        return Future<InputCallable, Continuation>(std::move(_input),
-//    std::move(callable));
-//    }
-//    */
-//
-//    auto go() {
-//        auto inputResult = Future<InputCallable>::go();
-//        return _input2(inputResult);
-//    }
-//
-//   private:
-//    // InputCallable _input;
-//    InputCallable2 _input2;
-//};
-//
-// template <typename InputCallable, typename InputCallable2,
-//          typename InputCallable3>
-// class Future3 : public Future2<InputCallable, InputCallable2> {
-//   public:
-//    Future3(InputCallable &&input, InputCallable2 &&input2,
-//            InputCallable3 &&input3)
-//        : Future2<InputCallable, InputCallable2>(std::move(input)),
-//          _input(std::move(input3)) {}
-//
-//    /*
-//    template <typename Continuation>
-//    auto then(Continuation && callable) {
-//        return Future<InputCallable, Continuation>(std::move(_input),
-//    std::move(callable));
-//    }
-//    */
-//
-//    auto go() {
-//        auto inputResult = Future2<InputCallable, InputCallable2>::go();
-//        return _input(inputResult);
-//    }
-//
-//   private:
-//    // InputCallable _input;
-//    InputCallable3 _input;
-//};
-//
-
-// class Future {
-//    public:
-//     Future() {}
-//
-//     template <typename Continuation>
-//     auto then(Continuation &&continuation);
-//
-//     auto go() { return _callable(); }
-//
-//    private:
-//     // InputCallable _input;
-//     MyCallable _callable;
-// };
-
-// template <typename MyCallable>
-// class Future {
-//    public:
-//     Future(MyCallable &&myCallable) : _callable(std::move(myCallable)) {}
-//
-//     template <typename Continuation>
-//     auto then(Continuation &&continuation) {
-//         return Future<Continuation, MyCallable>(
-//             std::forward<Continuation>(continuation), std::move(_callable));
-//     }
-//     auto go() { return _callable(); }
-//
-//    private:
-//     // InputCallable _input;
-//     MyCallable _callable;
-// };
-//
-
-// template <typename... Callables>
-// class Future {};
-//
-// template <typename MyCallable, typename... Callables>
-// class Future;
-
-// template <typename MyCallable, typename... Callables>
-// class Future : public Future<Callables...> {
-//   public:
-//    Future(MyCallable &&myCallable, Callables &&... callables)
-//        : Future<Callables...>(std::forward<Callables...>(callables...)),
-//          _callable(std::move(myCallable)) {}
-//
-//    template <typename Continuation>
-//    auto then(Continuation &&continuation) && {
-//        return Future<Continuation, MyCallable, Callables...>(
-//            std::forward<Continuation>(continuation), std::move(_callable));
-//    }
-//
-//    auto go() {
-//        auto inputResult = Future<Callables...>::go();
-//        return _callable(inputResult);
-//    }
-//
-//   private:
-//    // InputCallable _input;
-//    MyCallable _callable;
-//};
-//
-// template <typename MyCallable>
-// class Future<MyCallable> {
-//   public:
-//    Future(MyCallable &&myCallable)
-//        : _callable(std::forward<MyCallable>(myCallable)) {}
-//
-//    template <typename Continuation>
-//    auto then(Continuation &&continuation) && {
-//        return Future<Continuation, MyCallable>(
-//            std::forward<Continuation>(continuation),
-//            std::forward<MyCallable>(_callable));
-//    }
-//
-//    auto go() { return _callable(); }
-//
-//   private:
-//    // InputCallable _input;
-//    MyCallable _callable;
-//};
-
-//
-// template <typename MyCallable>
-// class Future<MyCallable, void> {
-//   public:
-//    Future(MyCallable &&myCallable)
-//        : _callable(std::forward<MyCallable>(myCallable)) {}
-//
-//    template <typename Continuation>
-//    auto then(Continuation &&continuation) && {
-//        return Future<Continuation, std::decay_t<decltype(*this)>>(
-//            std::forward<Continuation>(continuation), std::move(*this));
-//    }
-//
-//    auto go() { return _callable(); }
-//
-//   private:
-//    // InputCallable _input;
-//    MyCallable _callable;
-//};
-
-// template <typename MyCallable, typename InputCallable, typename
-// PreviousInputCallable> class Future : public Future<InputCallable,
-// PreviousInputCallable> {
-//   public:
-//    Future(MyCallable &&myCallable, InputCallable& callable)
-//        : Future<InputCallable>(std::forward<InputCallable>(callable)),
-//          _callable(std::move(myCallable)) {}
-//
-//    template <typename Continuation>
-//    auto then(Continuation &&continuation) && {
-//        return Future<Continuation, MyCallable, InputCallable>(
-//            std::forward<Continuation>(continuation), _callable);
-//    }
-//
-//    auto go() {
-//        auto inputResult = Future<Callables...>::go();
-//        return _callable(inputResult);
-//    }
-//
-//   private:
-//    // InputCallable _input;
-//    MyCallable _callable;
-//};
-
-// template <typename MyCallable, typename InputCallable>
-// class Future : public Future<InputCallable> {
-//   public:
-//    Future(MyCallable &&myCallable, InputCallable& callable)
-//        : Future<InputCallable>(std::forward<InputCallable>(callable)),
-//          _callable(std::move(myCallable)) {}
-//
-//    template <typename Continuation>
-//    auto then(Continuation &&continuation) && {
-//        return Future<Continuation, MyCallable, InputCallable>(
-//            std::forward<Continuation>(continuation), _callable);
-//    }
-//
-//    auto go() {
-//        auto inputResult = Future<Callables...>::go();
-//        return _callable(inputResult);
-//    }
-//
-//   private:
-//    // InputCallable _input;
-//    MyCallable _callable;
-//};
-//
-// template <typename MyCallable>
-// class Future<MyCallable> {
-//   public:
-//    Future(MyCallable &&myCallable)
-//        : _callable(std::forward<MyCallable>(myCallable)) {}
-//
-//    template <typename Continuation>
-//    auto then(Continuation &&continuation) && {
-//        return Future<Continuation, MyCallable>(
-//            std::forward<Continuation>(continuation),
-//            std::forward<MyCallable>(_callable));
-//    }
-//
-//    auto go() { return _callable(); }
-//
-//   private:
-//    // InputCallable _input;
-//    MyCallable _callable;
-//};
-
-// template <>
-// class Future<std::function<void(void)>> {
-//   public:
-//    Future(std::function<void(void)> &&) {
-//    }  //: _callable(std::move(myCallable)) {}
-//
-//    template <typename Continuation>
-//    auto then(Continuation &&continuation) {
-//        return Future<Continuation>(std::forward<Continuation>(continuation));
-//    }
-//
-//    auto go() { return 0; }
-//
-//   private:
-//    //    MyCallable _callable;
-//};
-//
-
 class Executor {
     using Callable = std::function<void()>;
 
    public:
     void schedule(Callable &&callable) {
-        // std::cout << "Scheduling" << std::endl;
         queue.emplace_back(std::move(callable));
     }
 
@@ -292,7 +14,6 @@ class Executor {
         while (!queue.empty()) {
             auto &next = queue.front();
             try {
-                //       std::cout << "Processing next" << std::endl;
                 next();
             } catch (...) {
             }
@@ -433,28 +154,6 @@ auto makeFuture(Callable &&callable) {
     return Future<Callable>(std::forward<Callable>(callable));
 }
 
-// auto voidThenValue(Executor &executor) {
-//    return makeFuture([]() { std::cout << "step 0" << std::endl; })
-//        .then([&executor]() {
-//            std::cout << "step 1" << std::endl;
-//            return 2;
-//        })
-//        .then([](int &&i) {
-//            //           std::cout << "step 2" << std::endl;
-//            std::cout << "in a future with i : " << i << std::endl;
-//            return std::string("something");
-//        })
-//        .then([](std::string &&s) {
-//            //            std::cout << "step 3" << std::endl;
-//            std::cout << "input s:  " << s << std::endl;
-//        })
-//        .then([](std::string &&s) {
-//            //            std::cout << "step 3" << std::endl;
-//            std::cout << "input s:  " << s << std::endl;
-//        })
-//        .go(executor);
-//}
-
 auto makeTestFutureChain(Executor &executor) {
     return makeFuture([]() { std::cout << "step 0" << std::endl; })
         .then([&executor]() {
@@ -476,9 +175,8 @@ auto makeTestFutureChain(Executor &executor) {
         })
         .then([]() { std::cout << "step 4" << std::endl; });
 }
-int main() {
-    // std::cout << "Hello World!\n";
 
+int main() {
     Executor executor;
     for (int i = 0; i < 3; ++i) {
         makeTestFutureChain(executor).go(executor);
@@ -486,31 +184,3 @@ int main() {
 
     executor.run();
 }
-// for (int i = 0; i < 100000; ++i) {
-//    std::cout << "Something asdfasdf" << i << std::endl;
-//    std::cout << "Something asdfasdf" << i << std::endl;
-//    std::cout << "Something asdfasdf" << i << std::endl;
-//    std::cout << "Something asdfasdf" << i << std::endl;
-//    std::cout << "Something asdfasdf" << i << std::endl;
-//}
-
-//    makeTestFutureChain(executor).go(executor);
-//    makeTestFutureChain(executor).go(executor);
-
-// makeFuture()
-//    .then([]() {
-//        std::cout << "step 1" << std::endl;
-//        return 2;
-//    })
-//    .then([](int i) {
-//        std::cout << "in a future with i : " << i << std::endl;
-//    })
-//    .then([] {
-//        std::cout << "in a continuation:  " << std::endl;
-//        return std::string("something");
-//    })
-//    .then(
-//        [](std::string &s) { std::cout << "input s:  " << s <<
-//        std::endl;
-//        })
-//    .go();
