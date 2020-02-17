18f3c2be92194c66d9b5560e869fd552b4cb9b97
Matthew Saltz <matthew.saltz@mongodb.com>
2020-02-17 13:48:16 -0500

    rm main2.cpp

--- main.cpp
+++ main.cpp
@@ -1,278 +1,307 @@
-#include <functional>
 #include <iostream>
 #include <list>
+#include <optional>
+#include <queue>
+#include <thread>
 #include <type_traits>
+#include <unordered_map>
+#include <variant>
 
-class Executor {
-    using Callable = std::function<void()>;
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/event.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
 
-   public:
-    void schedule(Callable &&callable) {
-        queue.emplace_back(std::move(callable));
+#include <netdb.h>
+#include <unistd.h>
+#include <cstdlib>
+
+#include "kernel_event_listener.h"
+#include "strand.h"
+#include "user_input_subscription_service.h"
+
+int tcpopen(const char* host, int port) {
+    struct sockaddr_in server;
+    struct hostent* hp;
+    int sckfd;
+
+    if ((hp = gethostbyname(host)) == NULL) diep("gethostbyname()");
+
+    if ((sckfd = socket(PF_INET, SOCK_STREAM, 0)) < 0) diep("socket()");
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    server.sin_addr = *((struct in_addr*)hp->h_addr);
+    memset(&(server.sin_zero), 0, 8);
+
+    if (connect(sckfd, (struct sockaddr*)&server, sizeof(struct sockaddr)) < 0)
+        diep("connect()");
+
+    std::cout << "sckfd: " << sckfd << std::endl;
+
+    return sckfd;
+}
+
+int tcpbind(const char* host, int port) {
+    struct sockaddr_in server;
+    struct hostent* hp;
+    int sckfd;
+
+    if ((hp = gethostbyname(host)) == NULL) diep("gethostbyname()");
+
+    if ((sckfd = socket(PF_INET, SOCK_STREAM, 0)) < 0) diep("socket()");
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    server.sin_addr = *((struct in_addr*)hp->h_addr);
+    memset(&(server.sin_zero), 0, 8);
+
+    int on = 1;
+    setsockopt(sckfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+    if (bind(sckfd, (struct sockaddr*)&server, sizeof(server)) == -1) {
+        perror("bind");
+        return 1;
     }
+    // TODO handle backlog in stream
+    assert(listen(sckfd, 5) != -1);
 
-    void run() {
-        while (!queue.empty()) {
-            auto &next = queue.front();
-            try {
-                next();
-            } catch (...) {
-            }
-            queue.pop_front();
-        }
-    }
+    std::cout << "sckfd: " << sckfd << std::endl;
 
-   private:
-    std::list<Callable> queue;
-};
+    return sckfd;
+}
 
-template <typename T>
-struct IsFuture;
+void sendbuftosck(int sckfd, const char* buf, int len) {
+    int bytessent, pos;
 
-template <typename T, typename Continuation>
-class AwaitableWithContinuation {
-    explicit AwaitableWithContinuation(Executor &e, Continuation &&continuation)
-        : _executor(e),
-          _continuation(std::forward<Continuation>(continuation)) {}
+    pos = 0;
+    do {
+        if ((bytessent = send(sckfd, buf + pos, len - pos, 0)) < 0)
+            diep("send()");
+        pos += bytessent;
+    } while (bytessent > 0);
+}
 
-    void set(T &&t) {
-        if (!_hasValue) {
-            _hasValue = true;
-            _t = t;
-            _executor.schedule(
-                [val = std::move(_t), continuation = std::move(_continuation)] {
-                    return continuation(val);
+void runServer(KernelEventListener& listener) {
+    auto sockfd = tcpbind("0.0.0.0", 8000);
+
+    listener.subscribe(sockfd, KernelEventListener::EventType::kRead)
+        .then([&listener, sockfd](struct kevent kev) {
+            std::cout << "Received new connection!" << std::endl;
+
+            struct sockaddr_in cliaddr;
+            socklen_t cliaddrlen = sizeof(cliaddr);
+
+            int connfd =
+                accept(sockfd, (struct sockaddr*)&cliaddr, &cliaddrlen);
+
+            std::cout << "Connection accepted!" << std::endl;
+
+            listener.subscribe(connfd, KernelEventListener::EventType::kRead)
+                .then([connfd](struct kevent kev) {
+                    const int kBufSize = 1024;
+                    char buf[kBufSize];
+
+                    // Read data from the client.
+                    memset(buf, 0, kBufSize);
+                    if (read(connfd, buf, kBufSize) < 0) diep("read()");
+                    // Echo it back.
+                    // TODO make also async
+                    if (write(connfd, buf, kBufSize) < 0) diep("write()");
+
+                    std::cout << "Client said: " << std::string(buf, kBufSize)
+                              << std::endl;
                 });
-        }
-    }
-
-   private:
-    bool _hasValue{false};
-    T _t;
-    Continuation _continuation;
-    // TODO &
-    Executor &_executor;
-};
-
-template <typename T>
-class Awaitable {
-   public:
-    explicit Awaitable(Executor &e) : _executor(e) {}
-
-    void set(T &&t) {
-        _hasValue = true;
-        _t = t;
-    }
-
-    template <typename Callable>
-    auto then(Callable &&continuation) {
-        if (_hasValue) {
-            _executor.schedule(
-                [val = std::move(_t),
-                 continuation = std::forward<Callable>(continuation)] {
-                    return continuation(val);
-                });
-        } else {
-            return AwaitableWithContinuation<T, Callable>(
-                _executor, std::forward<Callable>(continuation));
-        }
-    }
-
-   private:
-    bool _hasValue{false};
-    T _t;
-    // TODO &
-    Executor &_executor;
-};
-
-template <typename MyCallable, typename InputChain = std::nullptr_t>
-class Future {
-   public:
-    Future(MyCallable &&myCallable) : _callable(std::move(myCallable)) {}
-
-    Future(MyCallable &&myCallable, InputChain &&inputChain)
-        : _callable(std::forward<MyCallable>(myCallable)),
-          _inputChain(std::forward<InputChain>(inputChain)) {}
-
-    template <typename Continuation>
-    auto then(Continuation &&continuation) && {
-        using ThisType = std::decay_t<decltype(*this)>;
-
-        // if constexpr (std::is_void<decltype(go())>::value) {
-        // if constexpr (IsFuture<decltype(continuation())>::value) {
-        //    return Future(
-        //        [continuation] {
-        //            return continuation().then([](auto &x) { return x; });
-        //        },
-        //        std::forward<ThisType>(*this));
-
-        //    //            return this->then(
-        //    //                [this, continuation =
-        //    //                std::forward<Continuation>(continuation)](
-        //    //                    decltype(go()) &&x) { return
-        //    //                    x.then(continuation); });
-        //} else {
-        return Future<Continuation, ThisType>(
-            std::forward<Continuation>(continuation),
-            std::forward<ThisType>(*this));
-        //}
-        //}
-    }
-
-    auto go() {
-        if constexpr (std::is_null_pointer<InputChain>::value) {
-            return _callable();
-        } else {
-            if constexpr (std::is_void<decltype(_inputChain.go())>::value) {
-                _inputChain.go();
-                return _callable();
-            } else {
-                return _callable(_inputChain.go());
-            }
-        }
-    }
-
-    void go(Executor &executor) {
-        executor.schedule([this, &executor] {
-            if constexpr (std::is_null_pointer<InputChain>::value) {
-                return _callable();
-            } else {
-                if constexpr (std::is_void<decltype(_inputChain.go())>::value) {
-                    return _inputChain.goImpl(
-                        executor, [this]() mutable { return _callable(); });
-                } else {
-                    return _inputChain.goImpl(
-                        executor, [this](auto &&inputResult) mutable {
-                            return _callable(
-                                std::forward<decltype(inputResult)>(
-                                    inputResult));
-                        });
-                }
-            }
         });
+}
+
+using Buffer = std::vector<char>;
+
+class TCPConnection {
+   public:
+    TCPConnection(KernelEventListener& listener, int socket)
+        : _listener(listener), _socket(socket), _incomingMessages() {
+        listener.subscribe(socket, KernelEventListener::EventType::kRead)
+            .then([this, socket](const struct kevent& kev) {
+                auto bytesReady = kev.data;
+                Buffer buf(bytesReady);
+                if (::read(socket, buf.data(), bytesReady) < 0) {
+                    diep("read()");
+                }
+                if (_readWaiting) {
+                    _readWaiting->set(std::move(buf));
+                    _readWaiting.reset();
+                } else {
+                    _incomingMessages.emplace(std::move(buf));
+                }
+            });
     }
 
-    template <typename Callback>
-    auto goImpl(Executor &executor, Callback &&callback) {
-        if constexpr (std::is_null_pointer<InputChain>::value) {
-            if constexpr (std::is_invocable_v<Callback>) {
-                _callable();
-                executor.schedule([this, &executor, callback]() mutable {
-                    return callback();
-                });
-            } else {
-                auto result = _callable();
-                executor.schedule([this, &executor, callback,
-                                   result = std::move(result)]() mutable {
-                    return callback(std::move(result));
-                });
-            }
+    Future<Buffer> read(std::uint64_t numBytes) {
+        // If there's anything in _incomingMessages, pop it and return it
+        // immediately.
+        if (!_incomingMessages.empty()) {
+            Promise<Buffer> promise;
+            promise.set(std::move(_incomingMessages.front()));
+            _incomingMessages.pop();
+            return promise.getFuture();
         } else {
-            if constexpr (std::is_invocable_v<MyCallable>) {
-                if constexpr (std::is_invocable_v<Callback>) {
-                    return _inputChain.goImpl(
-                        executor, [this, &executor, callback]() mutable {
-                            _callable();
-                            executor.schedule(
-                                [this, &executor, callback]() mutable {
-                                    return callback();
-                                });
-                        });
-                } else {
-                    return _inputChain.goImpl(
-                        executor, [this, &executor, callback]() mutable {
-                            auto result = _callable();
-                            executor.schedule(
-                                [this, &executor, callback,
-                                 result = std::move(result)]() mutable {
-                                    return callback(std::move(result));
-                                });
-                        });
-                }
-            } else {
-                if constexpr (std::is_invocable_v<Callback>) {
-                    return _inputChain.goImpl(
-                        executor, [this, &executor,
-                                   callback](auto &&inputResult) mutable {
-                            _callable(std::forward<decltype(inputResult)>(
-                                inputResult));
-                            executor.schedule(
-                                [this, &executor, callback]() mutable {
-                                    return callback();
-                                });
-                        });
-
-                } else {
-                    return _inputChain.goImpl(
-                        executor, [this, &executor,
-                                   callback](auto &&inputResult) mutable {
-                            auto result =
-                                _callable(std::forward<decltype(inputResult)>(
-                                    inputResult));
-                            executor.schedule(
-                                [this, &executor, callback,
-                                 result = std::move(result)]() mutable {
-                                    return callback(std::move(result));
-                                });
-                        });
-                }
-            }
+            _readWaiting.emplace();
+            return _readWaiting->getFuture();
         }
     }
 
    private:
-    MyCallable _callable;
-    InputChain _inputChain;
+    KernelEventListener& _listener;
+    int _socket;
+    std::optional<Promise<Buffer>> _readWaiting;
+    std::queue<Buffer> _incomingMessages;
 };
 
-template <typename>
-struct IsFuture : public std::false_type {};
+void runClient(KernelEventListener& listener) {
+    auto serverSocket = tcpopen("0.0.0.0", 8000);
+    auto stdinfd = fileno(stdin);
 
-template <typename T>
-struct IsFuture<Future<T>> : public std::true_type {};
+    // Read data from stdin and send it to the server.
+    listener.subscribe(stdinfd, KernelEventListener::EventType::kRead)
+        .then([&listener, serverSocket](struct kevent kev) {
+            const int kBufSize = 1024;
+            char buf[kBufSize];
 
-template <class Callable>
-auto makeFuture(Callable &&callable) {
-    return Future<Callable>(std::forward<Callable>(callable));
+            memset(buf, 0, kBufSize);
+
+            if (read(fileno(stdin), buf, kBufSize) < 0) diep("read()");
+            // TODO make sense
+            if (write(serverSocket, buf, kBufSize) < 0) diep("write()");
+        });
+
+    listener.subscribe(serverSocket, KernelEventListener::EventType::kRead)
+        .then([serverSocket](struct kevent kev) {
+            std::cout << "received response from server" << std::endl;
+            const int kBufSize = 1024;
+            char buf[kBufSize];
+
+            // Read the response from the server.
+            memset(buf, 0, kBufSize);
+            if (read(serverSocket, buf, kBufSize) < 0) diep("read()");
+
+            std::cout << std::string(buf, kBufSize) << std::endl;
+        });
 }
 
-// Problem: Can't type the types!!!
+void testList() {
+    List l(3);
+    auto fullList = makeList(3).prepend("hi").prepend(4.2);
+    std::cout << "fullList.size(): " << fullList.size() << std::endl;
 
-// Future<> loop() {
-//    return makeFuture([]() { std::cout << "step 0" << std::endl; }).then([] {
-//        return loop();
-//    });
-//}
+    fullList.forEach([](auto& x) { std::cout << x << std::endl; });
 
-auto makeTestFutureChain(Executor &executor) {
-    return makeFuture([]() { std::cout << "step 0" << std::endl; })
-        .then([&executor]() {
-            std::cout << "step 1" << std::endl;
-            return makeFuture([]() {
-                std::cout << "step 1.1" << std::endl;
-                return 4;
-            });
-            //                .then([](int &&i) {
-            //                    std::cout << "step 1.2" << std::endl;
-            //                    return i;
-            //                });
-        })
-        .then([](auto &&fut) { std::move(fut).then([](int &&i) {}); });
-    //        .then([](int &&i) {
-    //            std::cout << "step 2, i : " << i << std::endl;
-    //            return std::string("something");
-    //        })
-    //        .then([](std::string &&s) {
-    //            std::cout << "step 3, s:  " << s << std::endl;
-    //        })
-    //        .then([]() { std::cout << "step 4" << std::endl; });
+    std::cout << "\n\n";
+
+    auto newList = makeList(4).append("hi").append(42);
+
+    newList.forEach([](auto& x) { std::cout << x << std::endl; });
+
+    std::cout << "\n\n";
+
+    auto listOfCallables =
+        makeList([] { std::cout << "First!" << std::endl; })
+            .append([] { std::cout << "Second!" << std::endl; })
+            .append([] { std::cout << "Third!" << std::endl; });
+
+    listOfCallables.forEach([](auto& callable) { callable(); });
+    listOfCallables.reverse().forEach([](auto& callable) { callable(); });
+
+    std::cout << "\n\n";
+
+    auto listOfNumbers = makeList(3).append(4.2).append(20ull);
+    auto sum = listOfNumbers.fold(
+        [](auto currentSum, auto next) { return currentSum + next; });
+
+    std::cout << "sum: " << sum << std::endl;
+
+    auto newListOfNumbersWithMoreNumbers =
+        std::move(listOfNumbers).appendAll(makeList(5).append(6).append(7));
+    std::cout << "newListOfNumbersWithMoreNumbers.size(): "
+              << newListOfNumbersWithMoreNumbers.size() << std::endl;
+    auto newSum = newListOfNumbersWithMoreNumbers.fold(
+        [](auto currentSum, auto next) { return currentSum + next; });
+    std::cout << "newSum: " << newSum << std::endl;
+
+    std::cout << "\n\n";
 }
 
-int main() {
-    Executor executor;
-    for (int i = 0; i < 3; ++i) {
-        makeTestFutureChain(executor).go(executor);
+void testKernelEventListener(Executor& executor,
+                             KernelEventListener& keventListener, int argc) {
+    if (argc == 1) {
+        runServer(keventListener);
+    } else if (argc > 1) {
+        std::cout << "Run client!" << std::endl;
+        runClient(keventListener);
     }
+}
 
+void testUserInputSubscriptionService(
+    Executor& executor, UserInputSubscriptionService& inputService) {
+    auto chain =
+        makeList([&] {
+            std::cout << "Process: Waiting to hear foo... " << std::endl;
+            return inputService.subscribe("foo");
+        })
+            .append([&](std::string s) {
+                std::cout << "Process: Saw input: " << s << std::endl;
+                std::cout << "Process: Waiting to hear bar...: " << std::endl;
+                return inputService.subscribe("bar");
+            })
+            .append([](std::string s) {
+                std::cout << "Process: Saw input: " << s << std::endl;
+            });
+
+    auto chain2 =
+        // Processes are your "lazy futures"
+        makeProcess([&] {
+            std::cout << "Process: Waiting to hear foo... " << std::endl;
+            // subscribe returns a Future<std::string> that gets flattened
+            return inputService.subscribe("foo");
+        })
+            .then([&](std::string s) {
+                std::cout << "Process: Saw input: " << s << std::endl;
+                return makeProcess([&] {
+                           std::cout << "Process: Waiting to hear bar...: "
+                                     << std::endl;
+                           return inputService.subscribe("bar");
+                       })
+                    .then([&](std::string s) {
+                        std::cout << "Nested process: stuff " << std::endl;
+                        return s;
+                    })
+                    .execute(executor);
+            })
+            .then([](std::string s) {
+                std::cout << "Process: Saw input: " << s << std::endl;
+                return 17;
+            });
+    for (auto i = 0; i < 10; ++i) {
+        // "Future" is just a one-off event trigger thing that takes a single
+        // continuation
+        Future<int> fut = chain2.execute(executor);
+        fut.then(
+            [](int i) { std::cout << "Process result: " << i << std::endl; });
+    }
+}
+
+int main(int argc, char** argv) {
+    Executor executor;
+
+    UserInputSubscriptionService inputService(executor);
+    inputService.run();
+    testUserInputSubscriptionService(executor, inputService);
+
+    KernelEventListener keventListener(executor);
+    keventListener.run();
+    testKernelEventListener(executor, keventListener, argc);
+
+    std::cout << "Starting executor" << std::endl;
     executor.run();
 }
